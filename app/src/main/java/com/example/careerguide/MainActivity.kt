555725d284d8bdb3fbced9a2e9629e14e089293a
ankitsingh4264 <singh.ankit2117@gmail.com>
2020-12-11 14:48:51 +0530

    nav graph implemented

--- app/src/main/java/com/example/careerguide/MainActivity.kt
+++ app/src/main/java/com/example/careerguide/MainActivity.kt
@@ -4,11 +4,12 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Rajeev
+
     }
 }
--- app/src/main/java/com/example/careerguide/login/LoginFrag.kt
+++ app/src/main/java/com/example/careerguide/login/LoginFrag.kt
@@ -1,14 +1,13 @@
 package com.example.careerguide.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.careerguide.R
 
-class LoginFragment : Fragment() {
-
+class LoginFrag : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -17,6 +16,9 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
 
     }
 }
