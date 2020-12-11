a3464a18cd415ba13c8f017b1f606b8875d31c87
iamrv-sachan <rajeevsachan1999@gmail.com>
2020-12-11 23:51:52 +0530

    sign up and log in with nav graph fixed and theme

--- app/src/main/java/com/example/careerguide/login/LoginFrag.kt
+++ app/src/main/java/com/example/careerguide/login/LoginFrag.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.careerguide.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFrag : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -17,8 +19,8 @@ class LoginFrag : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
-
+        sign_up.setOnClickListener {
+            view.findNavController().navigate(R.id.signUpFragment)
+        }
     }
 }
